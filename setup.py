4beba4434b6a80852193bce20f752a8c21991e92
John Beck <silocone@proton.me>
2025-02-20 12:28:25 -0500

    Add comma

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     """,
     long_description_content_type="text/markdown",
     url="https://github.com/silocone141/SoulsSaveCLI",
-    author="silocone"
+    author="silocone",
     include_package_data=True,
     install_requires=[
         'Click',
