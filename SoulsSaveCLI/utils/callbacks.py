39d5046029704d60708aad13caec175b4e101852
John Beck <silocone@proton.me>
2025-02-25 23:47:14 -0500

    Add try/except for getting data from configuration file

--- SoulsSaveCLI/utils/callbacks.py
+++ SoulsSaveCLI/utils/callbacks.py
@@ -8,16 +8,32 @@ def load_last(ctx, param, value):
     if not value or ctx.resilient_parsing:
         return
 
-    config_data = side_functions.get_data('config.yaml')
+    try:
+        config_data = side_functions.get_data('config.yaml')
+        last_load = config_data[3]
+        last_dest = config_data[0] + config_data[1]
 
-    last_load = config_data[3]
-    last_dest = config_data[0] + config_data[1]
+        if last_load == '':
+            click.echo(
+                "Last loaded save state not specified in configuration file"
+            )
 
-    if os.path.isfile(last_load):
-        shutil.copyfile(last_load, last_dest)
-        click.echo(f"Successfully loaded '{last_load}'")
+        elif os.path.isfile(last_load):
+            shutil.copyfile(last_load, last_dest)
+            click.echo(f"Successfully loaded '{last_load}'")
 
-    else:
-        click.echo(f"'{last_load}' does not exist. Use `soulsave list` for a list of available options.")
+        else:
+            click.echo(
+                f"'{last_load}' does not exist. Use `soulsave list` for a list "
+                "of available options."
+            )
 
-    ctx.exit()
+        ctx.exit()
+
+    except (FileNotFoundError, KeyError, TypeError):
+        click.echo(
+            "File 'config.yaml' does not exist or was not created properly. "
+            "Use 'soulsave init' to create the file."
+        )
+        quit()
+
