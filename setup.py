613da5615666e9aec96f8c110fbf8a3885d627da
John Beck <silocone@proton.me>
2025-11-08 14:40:57 -0500

    Formatting

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='soulsave-cli',
-    version='0.2.0',
+    name="soulsave-cli",
+    version="0.2.0",
     packages=find_packages(),
-    long_description=open('README.md').read(),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/silocone141/SoulsSaveCLI",
     author="silocone",
     license="MIT",
     include_package_data=True,
     install_requires=[
-        'Click',
-        'setuptools'
+        "Click",
+        "setuptools"
     ],
     entry_points={
-        'console_scripts': [
-            'soulsave = src:cli',
+        "console_scripts": [
+            "soulsave = src:cli",
         ],
     },
     extras_require={
