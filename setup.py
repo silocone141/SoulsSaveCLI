c2e95c1c7bf71608be3c7d746e72eb2aa292c5c0
John Beck <silocone@proton.me>
2025-11-11 16:30:34 -0500

    Add dependency: send2trash

--- setup.py
+++ setup.py
@@ -1,26 +1,18 @@
 from setuptools import setup, find_packages
 
-
-setup(
-    name="soulsave-cli",
-    version="0.2.0",
-    packages=find_packages(),
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/silocone141/SoulsSaveCLI",
-    author="silocone",
-    license="MIT",
-    include_package_data=True,
-    install_requires=[
-        "Click",
-        "setuptools"
-    ],
-    entry_points={
-        "console_scripts": [
-            "soulsave = src:cli",
-        ],
-    },
-    extras_require={
-        "dev": ["twine"]
-    }
-)
+setup(name="soulsave-cli",
+      version="0.2.0",
+      packages=find_packages(),
+      long_description=open("README.md").read(),
+      long_description_content_type="text/markdown",
+      url="https://github.com/silocone141/SoulsSaveCLI",
+      author="silocone",
+      license="MIT",
+      include_package_data=True,
+      install_requires=["Click", "send2trash", "setuptools"],
+      entry_points={
+          "console_scripts": [
+              "soulsave = src:cli",
+          ],
+      },
+      extras_require={"dev": ["twine"]})
