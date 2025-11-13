93cf1a75a77ee585146d6dd7709066c538fce0a3
John Beck <silocone@proton.me>
2025-11-12 22:06:21 -0500

    Formatting

--- src/commands/load.py
+++ src/commands/load.py
@@ -5,8 +5,13 @@ from src.utils import fetch
 
 
 @click.command()
-@click.option("--profile", "profile", type=str, required=True, prompt=True)
-@click.option("--name", "name", type=str, required=True, prompt=True)
+@click.option("--profile",
+              "-p",
+              "profile",
+              type=str,
+              required=True,
+              prompt=True)
+@click.option("--name", "-n", "name", type=str, required=True, prompt=True)
 def load(profile, name):
     """
     Load an existing save state
