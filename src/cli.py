3c91b9bd6608e3d6637d32074851f48248f1ebb2
John Beck <silocone@proton.me>
2025-11-30 01:24:14 -0500

    Add new command + logic for defaulting to ui if fzf is installed

--- src/cli.py
+++ src/cli.py
@@ -1,12 +1,19 @@
 import click
-from src.commands import add, init, list, load, new, trash
+import shutil
+from src.commands import add, init, list, load, new, trash, ui
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
-@click.group(no_args_is_help=True, context_settings=CONTEXT_SETTINGS)
-def cli():
-    pass
+@click.group(invoke_without_command=True, context_settings=CONTEXT_SETTINGS)
+@click.option("--profile", "-p", "profile", type=str)
+@click.pass_context
+def cli(ctx, profile):
+    if ctx.invoked_subcommand is None:
+        if shutil.which("fzf") is None:
+            click.echo(ctx.get_help())
+        else:
+            ctx.forward(ui.ui)
 
 
 cli.add_command(add.add)
@@ -15,6 +22,7 @@ cli.add_command(list.list)
 cli.add_command(load.load)
 cli.add_command(new.new)
 cli.add_command(trash.trash)
+cli.add_command(ui.ui)
 
 if __name__ == "__main__":
     cli()
