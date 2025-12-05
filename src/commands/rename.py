3ccd157cba8dde6e92b04533c0095fd45e906d47
John Beck <silocone@proton.me>
2025-12-05 00:13:56 -0500

    Initial commit

---
+++ src/commands/rename.py
@@ -0,0 +1,69 @@
+import click
+import os
+from src.utils import fetch
+
+
+def try_rename(old_path, new_path):
+    try:
+        os.rename(old_path, new_path)
+        click.echo("Rename succeeded.")
+
+    except OSError:
+        click.echo("New name is invalid. Rename operation was not successful.")
+
+
+@click.command()
+@click.option("--profile", "-p", "profile", type=str, required=True,
+              help="Profile name to be changed or profile of save state "
+                   "to be changed")
+@click.option("--save-state", "-s", "save_state", type=str,
+              help="Save state name to be changed")
+@click.option("--new-name", "-n", "new_name", type=str, prompt=True,
+              help="New name for profile/save state")
+@click.option("--yes", "-y", "yes", is_flag=True,
+              help="Do not ask for confirmation")
+def rename(profile, save_state, new_name, yes):
+    """
+    Rename a profile or save state
+    """
+
+    config_values = fetch.get_config_values(profile)
+    save_state_path = config_values["save_states"]
+    save_extension = os.path.splitext(config_values["profiles"][profile])[1]
+    profiles = config_values["profiles"]
+
+    if save_state:
+        save_file = fetch.resolve_save(profile, save_state_path, save_state,
+                                       save_extension)
+        if save_file is None:
+            click.echo(f"'{save_state}' does not exist. Use 'soulsave "
+                       f"list {profile}' to list available options.")
+            return
+
+        else:
+            new_save_file = os.path.join(save_state_path, profile, new_name +
+                                         save_extension)
+            if yes:
+                try_rename(save_file, new_save_file)
+
+            else:
+                if click.confirm(f"Action will rename '{save_file}' to "
+                                 f"'{new_save_file}'. Proceed?"):
+                    try_rename(save_file, new_save_file)
+
+    else:
+        profile_path = os.path.join(save_state_path, profile)
+        new_profile_path = os.path.join(save_state_path, new_name)
+
+        if yes:
+            try_rename(profile_path, new_profile_path)
+            profiles[new_name] = config_values["profiles"].pop(profile)
+            fetch.write_data(fetch.get_config_file(), config_values)
+
+        else:
+            if click.confirm(f"Action will rename '{profile_path}' to "
+                             f"'{new_profile_path}'.\nNote: This will also "
+                             f"update the configuration file. Proceed?"):
+                try_rename(profile_path, new_profile_path)
+                profiles[new_name] = config_values["profiles"].pop(profile)
+                fetch.write_data(fetch.get_config_file(), config_values)
