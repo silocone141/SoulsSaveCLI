c5a9cd9c7f90f757fd95acdbbd92595ed131535b
John Beck <silocone@proton.me>
2025-12-24 21:47:38 -0500

    Move custom prompt from fetch to prompts

--- src/commands/rename.py
+++ src/commands/rename.py
@@ -1,6 +1,6 @@
 import click
 import os
-from src.utils import fetch
+from src.utils import fetch, prompts
 
 
 def try_rename(old_path, new_path):
@@ -20,7 +20,7 @@ def try_rename(old_path, new_path):
 @click.option("--save-state", "-s", "save_state", type=str,
               help="Save state name to be changed")
 @click.option("--new-name", "-n", "new_name", type=str,
-              callback=fetch.get_change_parameter,
+              callback=prompts.get_change_parameter,
               help="New name for profile/save state")
 @click.option("--silent", "silent", is_flag=True,
               help="Do not ask for confirmation")
