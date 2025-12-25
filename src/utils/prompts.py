96b14199ad0852feb265216abaf5d881503b39ea
John Beck <silocone@proton.me>
2025-12-24 21:47:59 -0500

    Initial commit

---
+++ src/utils/prompts.py
@@ -0,0 +1,19 @@
+import click
+import os
+
+
+def get_change_parameter(ctx, param, value):
+    """
+    When a file/directory's name will be changed, show it in the rename prompt
+    """
+
+    if value is not None:
+        return value
+
+    profile = ctx.params.get("profile", None)
+    save_state = ctx.params.get("save_state", None)
+
+    if save_state is not None:
+        save_state = os.path.splitext(save_state)[0]
+
+    return click.prompt(f"Old name: {save_state or profile}\nNew Name: ")
